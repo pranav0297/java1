c6d31ecebebef7affa0c427dcb2634f87c4a9e66
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-09-22 00:29:24 +0530

    Update Char_percentage_4.java

--- Char_percentage_4.java
+++ Char_percentage_4.java
@@ -5,6 +5,10 @@ public class Char_percentage_4 {
         int integer=0;
         int other =0;
         char ch ;
+        double p_upper;
+        double p_lower;
+        double p_integer;
+        double p_other;
         for(int i =0;i<s.length();i++){
             ch = s.charAt(i);
 
@@ -24,7 +28,12 @@ public class Char_percentage_4 {
                 other++;
             }
         }
-        System.out.println("Upper case : "+upper+"\n"+"Lower case : "+lower+"\n"+"Integer : "+integer+"\n"+"Other : "+other);
+        p_upper = ((double)upper)/s.length()*100;
+        p_lower = ((double)lower)/s.length()*100;
+        p_integer = ((double)integer)/s.length()*100;
+        p_other = ((double)other)/s.length()*100;
+        
+        System.out.println("Upper case : "+upper+" ,"+p_upper+"\n"+"Lower case : "+lower+" ,"+p_lower+"\n"+"Integer : "+integer+" ,"+p_integer+"\n"+"Other : "+other+" ,"+p_other);
     }
     public static void main (String[] args) {
 
