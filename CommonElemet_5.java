3b84a720f09f1395d591b89c6f33d830716e8d87
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-09-22 01:19:28 +0530

    Update CommonElemet_5.java

--- CommonElemet_5.java
+++ CommonElemet_5.java
@@ -20,9 +20,18 @@ public class CommonElemet_5 {
 
     public static void main(String[] args)
     {
-        int[] arr1= { 1,2,3,4,5};
-
-        int[] arr2 = {3,4,5,3,6,7,8};
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input in array1: ");
+        for(int i=0;i<5;i++) {
+             arr1 [i] = sc.nextInt();
+        }
+        System.out.println("Enter the input in array2: ");
+        for(int i=0;i<5;i++) {
+            arr2 [i] = sc.nextInt();
+        }
+        
         CommonElemet_5 ce = new CommonElemet_5();
         System.out.println("Duplicate number are");
         ce.common(arr1, arr2);
